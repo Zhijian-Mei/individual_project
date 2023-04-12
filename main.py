8ebd25040578c15e7aea79ffb822fbfbbce9c7c7
Zhijian Mei <545902915@qq.com>
2023-04-12 11:10:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from datasets import load_dataset
 
 dataset = load_dataset('multi_nli')
 
-validation_matched = dataset['validation_matched']['premise']
+def f(x):
+    return x['premise'],x['hypothesis']
+
+
+validation_matched = dataset['validation_matched'].map(f,batched=True)
 validation_mismatched = dataset['validation_mismatched'].map(lambda x:{'premise':x['premise'],'hypothesis':x['hypothesis']})
 
 print(validation_matched)
