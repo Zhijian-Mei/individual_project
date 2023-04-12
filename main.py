4127e2b073917c515cc08cca8eba8df14537ae2b
Zhijian Mei <545902915@qq.com>
2023-04-12 11:23:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ dataset = load_dataset('multi_nli')
 validation_matched = dataset['validation_matched']
 validation_mismatched = dataset['validation_mismatched']
 torch.manual_seed(42)
+
+validation_matched = validation_matched[list(RandomSampler(validation_matched,num_samples=2500))]
+validation_mismatched = validation_mismatched[list(RandomSampler(validation_mismatched,num_samples=2500))]
+
 print(validation_matched)
-print(list(RandomSampler(validation_mismatched,num_samples=2500)))
