6ab41b58038954ea8f59241d907de8c9256a5e6d
Zhijian Mei <545902915@qq.com>
2023-04-12 11:22:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
 from torch.utils.data import RandomSampler
+import torch
 dataset = load_dataset('multi_nli')
 
 
@@ -7,6 +8,6 @@ dataset = load_dataset('multi_nli')
 
 validation_matched = dataset['validation_matched']
 validation_mismatched = dataset['validation_mismatched']
-
+torch.manual_seed(42)
 print(validation_matched)
 print(list(RandomSampler(validation_mismatched,num_samples=2500)))
