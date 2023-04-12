e2a3fec766b1f19e9ddd5d72c44e6d526556017f
Zhijian Mei <545902915@qq.com>
2023-04-12 11:20:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ validation_matched = dataset['validation_matched']
 validation_mismatched = dataset['validation_mismatched']
 
 print(validation_matched)
-print(dict(RandomSampler(validation_mismatched,num_samples=2500)))
+print(list(RandomSampler(validation_mismatched,num_samples=2500)))
